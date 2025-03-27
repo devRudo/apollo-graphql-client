501180a53c0205ee63820ec37d0a017d0954b1d6
Vijay Kumar Mishra <devrudo@gmail.com>
2025-03-27 22:58:44 +0530

    changes - project initialization with apollo client and graphql - query for fetching games - mutation for deleting a game

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,112 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { useQuery, useMutation, gql } from "@apollo/client";
+
+const GET_GAMES = gql`
+  query GetGames {
+    games {
+      id
+      title
+      platform
+    }
+  }
+`;
+
+const DELETE_GAME = gql`
+  mutation DeleteGame($id: ID!) {
+    deleteGame(id: $id) {
+      id
+      title
+      platform
+    }
+  }
+`;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { loading, error, data: gamesData } = useQuery(GET_GAMES);
+  const [
+    deleteGame,
+    { loading: deletingGame, error: errorDeletingGame, data },
+  ] = useMutation(DELETE_GAME, {
+    refetchQueries: [GET_GAMES],
+  });
+  console.log(loading, error, gamesData);
+  const { games } = gamesData || { games: [] };
+  const [activeGame, setActiveGame] = useState(null);
+
+  console.log(deletingGame, errorDeletingGame, data);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div style={{ padding: "1rem" }}>
+      {activeGame ? (
+        <div>
+          <button onClick={() => setActiveGame(null)}>&larr; Go Back</button>
+          <p>{activeGame.title}</p>
+          <p>{activeGame.platform?.join(", ")}</p>
+        </div>
+      ) : (
+        <>
+          <h1>Games</h1>
+          {loading ? (
+            <p>Loading...</p>
+          ) : error ? (
+            <p>{error.message}</p>
+          ) : games && games?.length > 0 ? (
+            <div style={{ display: "flex", flexWrap: "wrap", gap: 10 }}>
+              {games?.map((game) => {
+                return (
+                  <div
+                    style={{
+                      border: "1px solid white",
+                      padding: 10,
+                      borderRadius: 5,
+                      width: 300,
+                      cursor: "pointer",
+                      position: "relative",
+                    }}
+                    onClick={() => {
+                      setActiveGame(game);
+                    }}
+                  >
+                    <p>{game.title}</p>
+                    <p>{game.platform?.join(", ")}</p>
+                    <button
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        deleteGame({ variables: { id: game.id } });
+                      }}
+                    >
+                      Delete
+                    </button>
+                  </div>
+                );
+              })}
+              <div
+                style={{
+                  border: "1px solid white",
+                  padding: 10,
+                  borderRadius: 5,
+                  width: 300,
+                  cursor: "pointer",
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+                onClick={() => {
+                  // setActiveGame(game);
+                }}
+              >
+                <p>+ Add New Game</p>
+              </div>
+            </div>
+          ) : (
+            <p>No games found</p>
+          )}
+        </>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
